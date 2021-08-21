8a9ddc6554a29fa37a3aa2f5e07a365c4f232300
naomi <kiwijang5473@gmail.com>
2021-08-21 10:45:03 +0800

    新增dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent},
   { path: 'page1', component: Page1Component},
   { path: 'page2', component: Page2Component },
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { chartAreaDemo } from './chartAreaDemo';
-import { chartPieDemo } from './chartPieDemo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'demo1';
-
-  ngOnInit(): void {
-    chartAreaDemo();
-    chartPieDemo();
-  }
 }
